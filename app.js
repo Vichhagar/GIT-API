bedbc12e24b3b9a8f81e0c6b9b2657d1359baae0
vichhagar <greenraysvich@gmail.com>
2022-02-02 23:31:08 +0700

    adding search function

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const div = document.querySelector('.data');
+const form = document.querySelector('form');
+
 const getInfo = async(name) => {
     const respond = await fetch(`https://api.github.com/users/${name}`);
     const data = await respond.json();
@@ -11,25 +13,33 @@ const getRepos = async(url) => {
     return data;
 }
 
-getInfo('vichhagar').then(d => {
-    console.log(d.repos_url);
-    getRepos(d.repos_url).then(d => {
-        console.log(d)
-        printData(d);
-    }).catch(e => {
-        console.log(e)
-    });
-}).catch(e => {
-    console.log(e)
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    getData(form.username.value);
+    form.reset();
 })
 
+const getData = (username) => {
+    getInfo(username).then(d => {
+        console.log(d.repos_url);
+        getRepos(d.repos_url).then(d => {
+            console.log(d)
+            printData(d);
+        }).catch(e => {
+            console.log(e)
+        });
+    }).catch(e => {
+        console.log(e)
+    })
+}
+
+
 const printData = (data) => {
+    div.innerHTML = ' ';
     for (let i = 0; i <= data.length; i++) {
         div.innerHTML += 
         `
-        <ul>
-        <li>${data[i].name}</li>
-        </ul>
+            <li>${data[i].name}</li>
         `
     }
 
